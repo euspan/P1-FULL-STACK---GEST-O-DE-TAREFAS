0e26de4b39a66e6931b29728f4810b90176841a1
euspan <eumspan@gmail.com>
2026-09-23 20:41:37 -0300

    Corrige rotas do projeto principal e do app tarefas

--- tarefas/urls.py
+++ tarefas/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.inicio),
+    path('projetos/', views.listar_projetos),
+    path('pendentes/', views.listar_pendentes),
+    path('buscar/', views.buscar_tarefa),
+    path('status/', views.filtrar_status),
 ]
