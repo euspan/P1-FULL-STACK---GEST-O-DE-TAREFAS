467b728655f8de7ab058c0ead47fba872e9828be
euspan <eumspan@gmail.com>
2026-09-23 20:19:48 -0300

    Corrige import de views em tarefas/urls.py

--- tarefas/urls.py
+++ tarefas/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path
-from .. import views
+from . import views
 
 urlpatterns = [
     path('', views.inicio),
-    path('buscar/', views.buscar_tarefa),
-    path('status/', views.filtrar_status),
 ]
-
