defc846f6aad0784418c006b98f44e6a8ada5092
euspan <eumspan@gmail.com>
2026-09-23 20:10:10 -0300

    Cria página inicial com links para todas as funcionalidades

--- tarefas/views.py
+++ tarefas/views.py
@@ -2,16 +2,14 @@ from django.http import HttpResponse
 
 
 def inicio(request):
-    return HttpResponse('Olá, gestor de tarefas!')
-
-def buscar_tarefa(request):
-    titulo_busca = request.GET.get('titulo', '')
-    resultados = Subtarefa.objects.filter(titulo__icontains=titulo_busca)
-    texto = ', '.join(str(s) for s in resultados)
-    return HttpResponse(f'Resultado da busca: {texto}')
-
-def filtrar_status(request):
-    status_busca = request.GET.get('status', 'a fazer')
-    resultados = Subtarefa.objects.filter(status=status_busca)
-    texto = ', '.join(str(s) for s in resultados)
-    return HttpResponse(f'Subtarefas "{status_busca}": {texto}')
+    html = '''
+    <h1>Gestor de Tarefas</h1>
+    <ul>
+        <li><a href="/admin/">Django Admin</a></li>
+        <li><a href="/projetos/">Listar Projetos</a></li>
+        <li><a href="/pendentes/">Listar Subtarefas Pendentes</a></li>
+        <li><a href="/buscar/?titulo=">Buscar Subtarefa por Título</a></li>
+        <li><a href="/status/?status=a fazer">Filtrar por Status</a></li>
+    </ul>
+    '''
+    return HttpResponse(html)
