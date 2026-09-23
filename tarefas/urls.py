31a91b30de02a33457973ba3460c4c7a9a75b975
euspan <eumspan@gmail.com>
2026-09-23 19:51:57 -0300

    Adiciona busca de subtarefa por título

--- tarefas/urls.py
+++ tarefas/urls.py
@@ -3,4 +3,6 @@ from .. import views
 
 urlpatterns = [
     path('', views.inicio),
+    path('buscar/', views.buscar_tarefa),
 ]
+
--- tarefas/views.py
+++ tarefas/views.py
@@ -3,3 +3,8 @@ from django.http import HttpResponse
 
 def inicio(request):
     return HttpResponse('Olá, gestor de tarefas!')
+def buscar_tarefa(request):
+    titulo_busca = request.GET.get('titulo', '')
+    resultados = Subtarefa.objects.filter(titulo__icontains=titulo_busca)
+    texto = ', '.join(str(s) for s in resultados)
+    return HttpResponse(f'Resultado da busca: {texto}')
